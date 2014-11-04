e1d9926f146659c8677264e612fd1007e241b84e
fromradio <ruimin.wang13@gmail.com>
2014-11-04 11:50:18 +0800

    Numerical differential computation for vector

--- include/Frame/NumDifferential.h
+++ include/Frame/NumDifferential.h
@@ -6,17 +6,18 @@
 	compute the differential of functions
 */
 namespace COPT{
+
 template<class SFunc>
 class ScalarDifferential{
 private:
 	// the type of float number
 	typedef typename SFunc::FT 			FT;
+
 	// the reference to the scalar function
 	const SFunc&						__func;
+
 	// the error threshold
 	FT 									__epsilon;
-
-
 	// the iteration number that is used
 	// this parameter is only used in dev version
 	int 								__iterused;
@@ -24,16 +25,23 @@ private:
 	/*
 		private Functions
 	*/
+
 	// central differentce
 	FT centerDifference (FT x,FT h){
 		return (__func(x+h)-__func(x-h))/(2*h);
 	}
+
+	// second order differential
+	FT secondDifference (FT x,FT h){
+		return (__func(x+h)+__func(x-h)-2*__func(x))/(h*h);
+	}
+
 public:
 	/*
-		Constructor:
-			no default constructor is allowed
-			one must specify the function that is used
-	*/
+	 *	Constructor:
+	 *		no default constructor is allowed
+	 *		one must specify the function that is used
+	 */
 	ScalarDifferential(const SFunc& func,FT epsilon = 1e-5)
 		:
 		__func(func),
@@ -60,6 +68,98 @@ public:
 		return currdiff;
 	}
 
+	// compute second order differential
+	FT sDiff(FT x,FT h = 0.01){
+		FT error 			= __epsilon + 1.0;
+		FT forwarddiff 		= secondDifference(x,h);
+		FT currdiff 		= forwarddiff;
+		__iterused 			= 0;
+		while (error > __epsilon ){
+			h /= 2.0;
+			forwarddiff = currdiff;
+			currdiff 	= secondDifference(x,h);
+			error 		= fabs(currdiff - forwarddiff);
+			if ( ++ __iterused > 20 ){
+				std::cerr<<"Reach the max iteration number, differential result might be inaccurate"<<std::endl;
+				break;
+			}
+		}
+		return currdiff;
+	}
+
+};
+
+
+template<class VFunc>
+class VectorDifferential{
+private:
+	typedef typename VFunc::Vector 					Vector;
+	typedef typename Vector::FT 					FT;
+	/*
+	 *		const reference to the function
+	 */
+	const VFunc& 									__vfunc;
+	// 
+	FT 												__epsilon;
+	//
+	int 											__iterused;
+
+	/*
+	 *				Private Function
+	 */
+	FT centerDifference(const Vector& vec,FT h,int i){
+		Vector vp(vec),vm(vec);
+		vp[i] += h;
+		vm[i] -= h;
+		return (__vfunc(vp)-__vfunc(vm))/(2*h);
+	}
+
+	/*
+	 *
+	 *
+	 *
+	 */
+	FT computeDiff(const Vector& vec,int i,FT h = 0.01){
+		FT error 		= __epsilon+1.0;
+		FT forwarddiff 	= centerDifference(vec,h,i);
+		FT currdiff		= forwarddiff;
+		__iterused		= 0;
+		while ( error > __epsilon ){
+			h /= 2.0;
+			forwarddiff = currdiff;
+			currdiff 	= centerDifference(vec,h,i);
+			error 		= fabs(currdiff - forwarddiff);
+			if ( ++ __iterused > 20 ){
+				std::cerr<<"Reach the max iteration number, differential result might be inaccurate"<<std::endl;
+				break;
+			}
+		}
+		return currdiff;
+	}
+
+	/*
+	 *				compute differential of second order
+	 */
+	// FT secondDifference(const Vector& vec,FT h,int i,int j){
+	// 	Vector vp(vec),vm(vec);
+	// 	vp[i] +
+	// }
+public:
+	VectorDifferential(const VFunc& func,FT epsilon = 1e-5) 
+		: 
+		__vfunc(func),
+		__epsilon(epsilon)
+		{}
+	/*
+	 *			Main function, compute the gradient of '__func'
+	 */
+	Vector gradient(const Vector& vec,FT h = 0.01){
+	 	Vector result(vec.size());
+	 	for ( int i = 0 ; i < vec.size() ; ++ i ){
+	 		result[i] = computeDiff(vec,i,h);
+	 	}
+	 	return result;
+	 }
 };
 };
 
---
+++ makefile
@@ -0,0 +1,65 @@
+# makefile for optimization framework
+# written by Ruimin Wang
+# powered by 'MathU'
+# copyright@MathU
+
+DIR_INC += -I/usr/local/Cellar/Eigen/3.2.1/include/eigen3 -I./include/Frame -I./include/LeastSquares -I./include/FunctionRepository
+DIR_SRC += ./src/Frame ./src/LeastSquares ./src
+DIR_OBJ = ./obj
+DIR_BIN = ./bin
+
+SRC = $(foreach n, $(DIR_SRC),$(wildcard $(n)/*.cpp))
+# TT = $(patsubst %.cpp,%.o,$(SRC))
+# TT = $(notdir $(SRC))
+OBJ = $(patsubst %.cpp,$(DIR_OBJ)/%.o,$(notdir $(SRC)))
+
+TARGET = main
+BIN_TARGET = $(DIR_BIN)/$(TARGET)
+
+CC = g++
+CFLAGS = -g -Wall $(DIR_INC)
+
+vpath %.cpp ./src/Frame
+vpath %.cpp ./src/LeastSquares
+vpath %.cpp ./src
+
+#objects = test.o basicmath.o
+
+# INCLUDE += -I/usr/local/Cellar/Eigen/3.2.1/include/eigen3 -Iinclude/Frame/
+
+# vpath %.cpp include/Frame
+
+
+$(BIN_TARGET):$(OBJ)
+	$(CC) $(OBJ) -o $@
+
+$(DIR_OBJ)/%.o:%.cpp
+	$(CC) $(CFLAGS) -c $< -o $@
+
+.PHONY:clean
+clean:
+	find $(DIR_OBJ) -name *.o -exec rm -rf {} \;
+
+
+# all debug
+all:
+	@echo $(BIN_TARGET)
+	@echo $(SRC)
+	@echo $(notdir $(SRC))
+	@echo $(OBJ)
+	@echo $(TT)
+	@echo "end"
+
+
+#test : $(objects)
+#	$(CC) -o test $(objects)
+
+#test.o : include/Frame/test.cpp
+#	$(CC) -c $(INCLUDE) include/Frame/test.cpp
+
+#basicmath.o : include/Frame/basicmath.cpp include/Frame/basicmath.h
+#	$(CC) -c  $(INCLUDE) include/Frame/basicmath.cpp
+
+#.PHONY : clean
+#clean :
+#	-rm test $(objects)
