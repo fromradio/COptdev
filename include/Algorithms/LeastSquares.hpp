43aeff503dc3e984e55b274bbf427f06f415f34b
fromradio <ruimin.wang13@gmail.com>
2014-11-23 18:28:04 +0800

    code review of LeastSquares

--- include/Algorithms/LeastSquares.hpp
+++ include/Algorithms/LeastSquares.hpp
@@ -8,21 +8,54 @@
 
 namespace COPT
 {
-/*
-*            Currently,three methods are contained:
-*            Least Mean Square Method
-*            Least Square Method
-*            Recursive Least Square Method
+/*			class "LeastSquaresSolver"
+*           Currently,three methods are contained:
+*           Least Mean Square Method
+*           Least Square Method
+*           Recursive Least Square Method
 */
 template<class Scalar>
 class LeastSquaresSolver
 {
 public:
+	/** enum: type of algorithms */
 	enum SolverType{
 		LMS,       //Least Mean Square method
 		LS,        //Least Square method
 		RLS,       //Recursive Least Square method
 	};
+
+private:
+	/** private variables */
+	//%{
+	/** the coefficient matrix */
+	const MatrixBase<Scalar>&   __A;
+
+	/** constant vector */
+	const VectorBase<Scalar>&   __b;
+
+	/** parameter of Least Mean Square method */
+	Scalar                      __mu;
+
+	/** parameter of Recursive Least Square method */
+	Scalar                      __lam;
+
+	/** parameter of Recursive Least Square method */
+	Scalar                      __delta;
+
+	/** the final result */
+	VectorBase<Scalar>          __x;
+
+	/** the type of the solver,LMS as default */
+	SolverType                  __type;
+
+	//%} end of variables
+
+    /** the main algorithm */
+	void            doSolve();	
+
+	/** constructor and deconstructor */
+	//%{
 	LeastSquaresSolver(
 		const MatrixBase<Scalar>& A,
 		const VectorBase<Scalar>& b,
@@ -31,72 +64,111 @@ public:
 		const Scalar delta = 250,
 		const SolverType type = LMS
 		);
+	//%} end of constructor and deconstructor
 
-	// set the type of the solver:LMS,LS or RLS
+	/** setter and getter */
+	//%{
+	/** set the type of the solver:LMS,LS or RLS */
 	void setType(const SolverType type);
 
-    // interface for solving the problem
+    /** interface for solving the problem */
 	void solve(VectorBase<Scalar>& x);
 
-	// print the solver information
+	//%}
+
+	/** print the solver information */
 	void printInfo();
 
-	// return the result
+	/** return the result */
 	const VectorBase<Scalar>& result() const{return __x;}
 
-private:
-	// the coefficient matrix
-	const MatrixBase<Scalar>&   __A;
-	// constant vector
-	const VectorBase<Scalar>&   __b;
-	// parameter of Least Mean Square method
-	Scalar                      __mu;
-	// parameter of Recursive Least Square method
-	Scalar                      __lam;
-	// parameter of Recursive Least Square method
-	Scalar                      __delta;
-	// the final result
-	VectorBase<Scalar>          __x;
-	//the type of the solver,LMS as default
-	SolverType                  __type;
 
-    //the main algorithm
-	void            doSolve();
 
 public:
-	void LeastMeanSquareMethod(
+	/** algorithms */
+	//%{
+
+	/*
+	 *    Least Mean Square algorithm
+	 *    /param A:          the coefficient matrix
+	 *    /param b:          constant vector
+	 *    /param mu:         step size
+	 *    /param x:          weight we want to find
+	 */
+	static inline void LeastMeanSquareMethod(
 		const MatrixBase<Scalar>& A,
 		const VectorBase<Scalar>& b,
 		const Scalar mu,
 		VectorBase<Scalar>& x
 		);
-	void LeastSquareMethod(
+
+	/*
+	 *    Least Square Method
+	 *    /param A:          the coefficient matrix
+	 *    /param b:          constant vector
+	 *    /param x:          weight we want to find
+	 */
+	static inline void LeastSquareMethod(
 		const MatrixBase<Scalar>& A,
 		const VectorBase<Scalar>& b,
 		VectorBase<Scalar>& x
 		);
-	void RLS_Method(
+
+	/*
+	 *    Recursive Least Square algorithm 
+	 *    /param A:         the coefficient matrix
+	 *    /param b:         constant vector
+ 	 *    /param x:         weight we want to find   
+	 *    /param lam:       related to the forgetting factor
+	 *    /param delta:     in order to define P(0)   
+	 */
+	static inline void RLS_Method(
 		const MatrixBase<Scalar>& A,
 		const VectorBase<Scalar>& b,
 		VectorBase<Scalar>& x,
 		const Scalar lam = 1,
 		const Scalar delta = 250);
 
+	//%} end of algorithms
+
 private:
+
+	/** private functions */
+	//%{
+
+	/*
+	 *    Least Mean Square algorithm update step
+	 *    /param a:         row of the coefficient matrix
+	 *    /param b:         target value corresponding to a
+	 *    /param mu:        step size
+	 *    /param x:         weight we find after each step
+	 */
 	void LeastMeanSquareUpdate(
 	const VectorBase<Scalar>& a,
 	const Scalar b,
 	const Scalar mu,
 	VectorBase<Scalar>& x
 	);
+
+	/*
+	 *    Recursive Least Square algorithm update step
+	 *    /param a:         row of the coefficient matrix
+	 *    /param b:         target value corresponding to a
+	 *    /param x:         weight we find after each step   
+	 *    /param p:         a trival variable    
+	 *    /param lam:       related to the forgetting factor
+	 *    /param delta:     in order to define P(0)   
+	 */
 	void RLS_learning(
-	const VectorBase<Scalar>& a,
-	const Scalar b,
-	VectorBase<Scalar>& x,
-	MatrixBase<Scalar>& p,
-	const Scalar lam,
-	const Scalar delta
+		const VectorBase<Scalar>& a,
+		const Scalar b,
+		VectorBase<Scalar>& x,
+		MatrixBase<Scalar>& p,
+		const Scalar lam,
+		const Scalar delta
 	);
+
+	//%} end of private functions
 };
 
 
@@ -119,9 +191,7 @@ LeastSquaresSolver<Scalar>::LeastSquaresSolver(
 {
 }
 
-/*
-*        set the type of the solver: LMS , LS or RLS
-*/
+
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::setType(const SolverType type)
 {
@@ -129,9 +199,6 @@ void LeastSquaresSolver<Scalar>::setType(const SolverType type)
 }
 
 
-/*      Kernel function
-*      Solving the problem
-*/
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::doSolve()
 {
@@ -167,10 +234,6 @@ void LeastSquaresSolver<Scalar>::doSolve()
 	}
 }
 
-
-/*        Solver interface for users
-*         /param x:              inital point
-*/
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::solve(
 	VectorBase<Scalar>& x
@@ -205,20 +268,6 @@ void LeastSquaresSolver<Scalar>::printInfo()
 	std::cout<<__x<<std::endl;
 }
 
-#endif
-
-
-
-
-
-
-/*
-*    Least Mean Square algorithm update step
-*    /param a:         row of the coefficient matrix
-*    /param b:         target value corresponding to a
-*    /param mu:        step size
-*    /param x:         weight we find after each step
-*/
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::LeastMeanSquareUpdate(
 	const VectorBase<Scalar>& a,
@@ -231,13 +280,6 @@ void LeastSquaresSolver<Scalar>::LeastMeanSquareUpdate(
 	x = x + mu*e*a;
 }
 
-/*
-*    Least Mean Square algorithm
-*    /param A:          the coefficient matrix
-*    /param b:          constant vector
-*    /param mu:         step size
-*    /param x:          weight we want to find
-*/
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::LeastMeanSquareMethod(
 	const MatrixBase<Scalar>& A,
@@ -252,12 +294,6 @@ void LeastSquaresSolver<Scalar>::LeastMeanSquareMethod(
 
 }
 
-/*
-*    Least Square Method
-*    /param A:          the coefficient matrix
-*    /param b:          constant vector
-*    /param x:          weight we want to find
-*/
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::LeastSquareMethod(
 	const MatrixBase<Scalar>& A,
@@ -268,16 +304,6 @@ void LeastSquaresSolver<Scalar>::LeastSquareMethod(
 	x = (A.transpose()*A).solve(A.transpose()*b);
 }
 
-//Recursive Least Square
-/*
-*    Recursive Least Square algorithm update step
-*    /param a:         row of the coefficient matrix
-*    /param b:         target value corresponding to a
-*    /param x:         weight we find after each step   
-*    /param p:         a trival variable    
-*    /param lam:       related to the forgetting factor
-*    /param delta:     in order to define P(0)   
-*/
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::RLS_learning(
 	const VectorBase<Scalar>& a,
@@ -296,14 +322,7 @@ void LeastSquaresSolver<Scalar>::RLS_learning(
 	MatrixBase<Scalar> pp = k.mulTrans(pai); 
 	p = (1.0/lam)*(p - pp);
 }
-/*
-*    Recursive Least Square algorithm 
-*    /param A:         the coefficient matrix
-*    /param b:         constant vector
-*    /param x:         weight we want to find   
-*    /param lam:       related to the forgetting factor
-*    /param delta:     in order to define P(0)   
-*/
+
 template<class Scalar>
 void LeastSquaresSolver<Scalar>::RLS_Method(
 	const MatrixBase<Scalar>& A,
@@ -319,4 +338,7 @@ void LeastSquaresSolver<Scalar>::RLS_Method(
 		RLS_learning(A.row(i),b[i],x,p,lam,delta);
 }
 
-}
+}// End of namespace COPT
+
+
+#endif
