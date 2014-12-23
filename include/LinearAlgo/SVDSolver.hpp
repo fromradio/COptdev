7443182a7bcddcfdd510bd057c408f57fcd4985d
Ruimin Wang <ruimin.wang13@gmail.com>
2014-12-23 16:15:57 +0800

    svd solver is planned to be added

---
+++ include/LinearAlgo/SVDSolver.hpp
@@ -0,0 +1,22 @@
+//		Copyright (C) Ruimin Wang, ruimin.wang13@gmail.com
+// 		Copyright (C) MathU
+
+#ifndef SVD_SOLVER_HPP__
+#define SVD_SOLVER_HPP__
+
+
+namespace COPT
+{
+
+template<class Matrix>
+class SVD
+{
+private:
+	
+public:
+
+};
+
+}// End of namespace COPT
+
+#endif
