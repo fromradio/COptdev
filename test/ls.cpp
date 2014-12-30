fa5ab9f3c5a927a6c74f94ea0da5649f4149e7c3
GuoSongtao <stg@mail.ustc.edu.cn>
2014-12-30 03:49:01 -0500

    standardization

--- test/ls.cpp
+++ test/ls.cpp
@@ -9,10 +9,10 @@ typedef COPT::LeastSquaresProblem<kernel>	problem;
 typedef COPT::LeastMeanSquareSolver<problem,COPT::SolverTimeStatistics>	lmssolver;
 typedef COPT::LeastSquareSolver<problem,COPT::SolverTimeStatistics>	lssolver;
 typedef COPT::RecursiveLeastSquareSolver<problem,COPT::SolverTimeStatistics> 	rlssolver;
-
-
-const int m = 180;
-const int n = 360;
+ 
+  
+const int m = 100;
+const int n = 10;
 
 int main(int argc,char* argv[])
 {
@@ -20,11 +20,19 @@ int main(int argc,char* argv[])
 	Vector b;
 	A = Matrix::random(m,n);
 	b = Vector::random(m);
+	std::cout<<"A : "<<std::endl<<A<<std::endl;
+	std::cout<<"b : "<<std::endl<<b<<std::endl;
 	problem pro(A,b);
 	lmssolver lmssol(pro);
+	lssolver lssol(pro);
+	rlssolver rlssol(pro);
+    
+	// COPT::Printer::printType(A,std::cout);
+	// COPT::Printer::printType(b,std::cout);
 
-	COPT::Printer::printType(A,std::cout);
-
+    std::cout<<"results of three solvers:"<<std::endl;
 	lmssol.solve();
+	lssol.solve();
+	rlssol.solve();
 
 }
