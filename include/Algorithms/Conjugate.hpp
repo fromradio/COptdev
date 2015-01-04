8a30b8d59b4c39f5631537deff406f9f07d95d0e
Ruimin Wang <ruimin.wang13@gmail.com>
2015-01-04 17:07:16 +0800

    Add GPL license

--- include/Algorithms/Conjugate.hpp
+++ include/Algorithms/Conjugate.hpp
@@ -1,12 +1,24 @@
-
-//	This file is part of open library COPT
-//		Powered by 'MathU'
-// 		Copyright (c) MathU,
-//		Written by Ruimin Wang, ruimin.wang13@gmail.com
+// This file is part of COPT, a light-weight C++ based optimization open source library
 //
+// Copyright (C) 2015 Ruimin Wang <ruimin.wang13@gmail.com>
+// Copyright (C) 2015 MathU
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef CONJUGATE_GRADIENT_METHOD
-#define CONJUGATE_GRADIENT_METHOD
+
+#ifndef CONJUGATE_GRADIENT_METHOD_HPP__
+#define CONJUGATE_GRADIENT_METHOD_HPP__
 
 
 
