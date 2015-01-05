34aa9ce14c164232e4604afbd15285f76c95ab04
cbywxkj <chenby@mail.ustc.edu.cn>
2015-01-05 10:33:48 +0800

    add

--- test/nls.cpp
+++ test/nls.cpp
@@ -13,13 +13,23 @@ int main(int argc,char* argv[])
 { 
 
 	typedef COPT::VectorFunctionSystem<FT>	Function;
-	Function func(2,2);
-	Vector initial_x(2);
-	initial_x[0]=0;
-	initial_x[1]=0;
 
-	problem p(func, 2, 2);
+//	Function func(2,2);
+//	Vector initial_x(2);
+//	initial_x[0]=1;
+//	initial_x[1]=2;
+
+	Function func(4,4);
+	Vector initial_x(4);
+	initial_x[0]=0;
+	initial_x[1]= -1.5;
+	initial_x[2]= -1;
+	initial_x[3]=1.5;
+
+//	problem p(func, 2, 2);
+	problem p(func, 4, 4);
 	psolver lm(p, initial_x);
 	lm.solve();
 	lm.printInfo();
+	std::cout<<"The objective value is  "<<lm.objective()<<std::endl;
 }
