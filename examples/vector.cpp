87d2cd2c43d1ccb011e5c4d066426679c6603ad8
fromradio <ruimin.wang13@gmail.com>
2015-02-04 19:44:42 +0800

    fix vector example

--- examples/vector.cpp
+++ examples/vector.cpp
@@ -4,7 +4,7 @@
  *		'VectorBase'. 'VectorBase' is included in Header file 'Vector.hpp'
  * 		and feel free to read the header file for a detailed description.
  *		
- *		'VectorBase' describes widely-used type vector in optimization. 
+ *		'VectorBase' describes widely-used mathematical type vector. 
  *		User can use simple syntax to initialize, assign and operate vectors
  * 		in COPT. Matlab-like assignment is also implemented for people who
  *		are familiar with Matlab. Basic operations like summation, subtraction
@@ -20,7 +20,7 @@ typedef double 							scalar;
 typedef int 							ind;
 typedef COPT::VectorBase<scalar,ind> 	Vector;
 
-/** the size of vector in the example */
+/** the size of vector */
 constexpr int n = 5;
 
 int main(int argc,char *argv[])
@@ -29,37 +29,28 @@ int main(int argc,char *argv[])
 
 	/** initialize vector v1 based on a given array s */
 	Vector v1(5,s);
-	/** v1 should be [1.0,4.0,5.0,2.1,3.2] */
-	std::cout<<"v1 is "<<v1<<std::endl;
+	std::cout<<"v1 is "<<v1<<std::endl; // [1.0,4.0,5.0,2.1,3.2]
 
 	/** any initialized vector is assumed to be zero */
 	Vector v2(5);
-	/** v2 should be [0,0,0,0,0] */
-	std::cout<<"v2 is "<<v2<<std::endl;
+	std::cout<<"v2 is "<<v2<<std::endl; // [0,0,0,0,0]
 
 	/** Matlab-like assignment */
 	v2(0) = 0.1;
 	v2(3) = 5.1;
-	/** v2 should be [0.1,0,0,5.1,0] */
-	std::cout<<"v2 becomes "<<v2<<std::endl;
+	std::cout<<"v2 becomes "<<v2<<std::endl; // [0.1,0,0,5.1,0]
 
 	/** basic operations */
-	/** the result of summation should be [1.1,4.0,5.0,7.1,3.2] */
-	std::cout<<"v1+v2="<<v1+v2<<std::endl;
-	/** the result of dot operation should be 10.81 */
-	std::cout<<"(v1,v2)="<<v1.dot(v2)<<std::endl;
+	std::cout<<"v1+v2="<<v1+v2<<std::endl; 		// [1.1,4.0,5.0,7.1,3.2]
+	std::cout<<"(v1,v2)="<<v1.dot(v2)<<std::endl; // 10.81
 
 	/** computation of l2 norm of v1 */
-	/** result should be 7.52662 */
-	std::cout<<"l2 norm of v1 is "<<v1.norm()<<std::endl;
-	/** result shoudl be 5*/
-	std::cout<<"l0 norm of v1 is "<<COPT::norm(v1,0)<<std::endl;
+	std::cout<<"l2 norm of v1 is "<<v1.norm()<<std::endl; // 7.52662
+	std::cout<<"l0 norm of v1 is "<<COPT::norm(v1,'0')<<std::endl; // 5
 
 	/** vector blocking of v1 with indices 0 and 2 */
 	ind inds[]{0,2};
-	/** the result should be [1.0,5.0] */
-	std::cout<<"blocking of v1 is "<<v1.block(2,inds,inds+2)<<std::endl;
+	std::cout<<"blocking of v1 is "<<v1.block(2,inds,inds+2)<<std::endl; // [1.0,5.0]
 	v2.blockFromVector(v1,2,inds,inds+2);
-	/** v2 becomes [1.0,5.0] */
-	std::cout<<"v2 becomes "<<v2<<std::endl;
+	std::cout<<"v2 becomes "<<v2<<std::endl; // [1.0,5.0]
 }
